afcb8b2d634aba919ef93fd43f640152d7d19460
MurilloIDM <murilloidm05@gmail.com>
2020-10-29 22:22:02 -0300

    Update: Finaliza alteração de layout

--- src/main/resources/static/js/transitionSection.js
+++ src/main/resources/static/js/transitionSection.js
@@ -3,58 +3,27 @@ const tableData = document.querySelector('.table-data');
 const formRegister = document.querySelector('.form-register');
 const formUpdate = document.querySelector('.form-update');
 
-const componentsScreen = [tableData, formRegister, formUpdate];
+const enableOrDisableClass = (componentsScreens, idNumber) => {
+  componentsScreens.forEach((component, index) => {
+    if (index === idNumber) {
+      if (component.classList.contains('disabled')) {
+        component.classList.remove('disabled')
+      }
+      component.classList.add('activated')
+    } else {
+      if (component.classList.contains('activated')) {
+        component.classList.remove('activated')
+      }
+      component.classList.add('disabled')
+    }
+  });
+}
 
 menuBar.addEventListener('click', (e) => {
+  const componentsScreens = [tableData, formRegister, formUpdate];
   const idNumber = (e.target.id) ? Number(e.target.id) : null;
 
-  switch (idNumber) {
-    case 0:
-      componentsScreen.forEach((component, index) => {
-        if (index === idNumber) {
-          if (component.classList.contains('disabled')) {
-            component.classList.remove('disabled')
-          }
-          component.classList.add('activated')
-        } else {
-          if (component.classList.contains('activated')) {
-            component.classList.remove('activated')
-          }
-          component.classList.add('disabled')
-        }
-      })
-      break;
-    case 1:
-      componentsScreen.forEach((component, index) => {
-        if (index === idNumber) {
-          if (component.classList.contains('disabled')) {
-            component.classList.remove('disabled')
-          }
-          component.classList.add('activated')
-        } else {
-          if (component.classList.contains('activated')) {
-            component.classList.remove('activated')
-          }
-          component.classList.add('disabled')
-        }
-      })
-      break;
-    case 2:
-      componentsScreen.forEach((component, index) => {
-        if (index === idNumber) {
-          if (component.classList.contains('disabled')) {
-            component.classList.remove('disabled')
-          }
-          component.classList.add('activated')
-        } else {
-          if (component.classList.contains('activated')) {
-            component.classList.remove('activated')
-          }
-          component.classList.add('disabled')
-        }
-      })
-      break;
-    default:
-      break;
+  if (idNumber) {
+    enableOrDisableClass(componentsScreens, idNumber);
   }
-})
+});

3ab1b4d4a058b143f27ac9a634e6c253f47812dd
ElianMelo <elianmelomorais@gmail.com>
2021-01-04 23:01:55 -0300

    Adiciona forma de inserir itens em um pedido

--- src/main/java/com/sandropastelaria/omniorder/controller/PedidoController.java
+++ src/main/java/com/sandropastelaria/omniorder/controller/PedidoController.java
@@ -1,7 +1,13 @@
 package com.sandropastelaria.omniorder.controller;
 
+import com.sandropastelaria.omniorder.model.Pedido;
+
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class PedidoController {
@@ -12,8 +18,17 @@ public class PedidoController {
 	}
 	
 	@GetMapping("/pedido-adicionar")
-	public String exibeForm() {
+	public String exibeForm(Model modelo) {
+		modelo.addAttribute("pedido", new Pedido());
 		return "pedido/pedido-adicionar";
+	}
+
+	@RequestMapping(value = "/pedido-adicionar", params = {"addItem"}, method = RequestMethod.POST)
+	public String addItem(final Pedido pedido, @RequestParam("addItem") String addRow) {
+		Integer id = pedido.getIdMesa();
+		id += 1;
+		pedido.setIdMesa(id);
+		return "pedido/pedido-adicionar"; // retorno com a nova list
 	}
 	
 	@GetMapping("/pedido-atualizar")

e68e9e4dc7b50a1b66e28f47cb79b3062649f68b
ElianMelo <elianmelomorais@gmail.com>
2021-01-04 21:02:48 -0300

    Criação do dominio e dao

--- src/main/java/com/sandropastelaria/omniorder/dao/PedidoDAO.java
+++ src/main/java/com/sandropastelaria/omniorder/dao/PedidoDAO.java
@@ -1,5 +1,117 @@
 package com.sandropastelaria.omniorder.dao;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
+import com.sandropastelaria.omniorder.config.FabricaDeConexao;
+import com.sandropastelaria.omniorder.model.Pedido;
+
 public class PedidoDAO {
 
+	public void inserir(Pedido pedido) {
+		Connection conexao = FabricaDeConexao.getConnection();
+		PreparedStatement stmt;
+		String sql = "insert into pedido" + "(id_pedido, estado_pedido, estado_cozinha, id_mesa)"
+				+ " values (?,?,?,?)";
+		try {
+            stmt = conexao.prepareStatement(sql);
+			stmt.setInt(1, pedido.getIdPedido());
+			stmt.setString(2, pedido.getEstadoPedido());
+            stmt.setString(3, pedido.getEstadoCozinha());
+            stmt.setInt(4, pedido.getIdMesa());
+			stmt.execute();
+			stmt.close();
+			conexao.close();
+		} catch (Exception e) {
+            System.out.println(e);
+		}
+	}
+
+	public List<Pedido> todos() {
+		List<Pedido> lista = new ArrayList<Pedido>();
+		Connection conexao = FabricaDeConexao.getConnection();
+		String sql = "select * from pedido;";
+		try {
+			PreparedStatement stmt = conexao.prepareStatement(sql);
+
+			ResultSet retorno = stmt.executeQuery();
+			while (retorno.next()) {
+				Integer idPedido = retorno.getInt("id_pedido");
+				String estadoPedido = retorno.getString("estado_pedido");
+				String estadoCozinha = retorno.getString("estado_cozinha");
+				Integer idMesa = retorno.getInt("id_mesa");
+				
+				Pedido pedido = new Pedido(idPedido, estadoPedido, estadoCozinha, idMesa);
+				lista.add(pedido);
+			}
+			retorno.close();
+			stmt.close();
+			conexao.close();
+		} catch (SQLException e) {
+			System.out.println(e.getMessage());
+		}
+		return lista;
+	}
+
+	public void excluir(int id) {
+		Connection conexao = FabricaDeConexao.getConnection();
+		PreparedStatement stmt;
+		String sql = "delete from pedido where id_pedido = ?";
+		try {
+			stmt = conexao.prepareStatement(sql);
+			stmt.setInt(1, id);
+			stmt.execute();
+			stmt.close();
+			conexao.close();
+		} catch (Exception e) {
+			System.out.println(e.getMessage());
+		}
+	}
+
+	public void atualizar(Pedido pedido) {
+		Connection conexao = FabricaDeConexao.getConnection();
+		PreparedStatement stmt;
+		String sql = "update pedido set estado_pedido=?,estado_cozinha=?,id_mesa=?"
+				+ " where id_pedido = ?";
+		try {
+            stmt = conexao.prepareStatement(sql);
+            
+			stmt.setString(1, pedido.getEstadoPedido());
+			stmt.setString(2, pedido.getEstadoCozinha());
+			stmt.setInt(3, pedido.getIdMesa());
+            stmt.setInt(4, pedido.getIdPedido());
+
+			stmt.execute();
+			stmt.close();
+			conexao.close();
+		} catch (Exception e) {
+			System.out.println(e.getMessage());
+		}
+	}
+
+	public Pedido buscaPorId(int id) {
+		Connection conexao = FabricaDeConexao.getConnection();
+		String sql = "select * from pedido where id_pedido = ?;";
+		Pedido pedido = null;
+		try {
+			PreparedStatement stmt = conexao.prepareStatement(sql);
+			stmt.setInt(1, id);
+			ResultSet retorno = stmt.executeQuery();
+			retorno.next();
+			Integer idPedido = retorno.getInt("id_pedido");
+			String estadoPedido = retorno.getString("estado_pedido");
+            String estadoCozinha = retorno.getString("estado_cozinha");
+            Integer idMesa = retorno.getInt("id_mesa");
+			pedido = new Pedido(idPedido, estadoPedido, estadoCozinha, idMesa);
+			stmt.close();
+			conexao.close();
+		} catch (SQLException e) {
+			System.out.println(e.getMessage());
+		}
+		return pedido;
+	}
 }
--- src/main/java/com/sandropastelaria/omniorder/model/Pedido.java
+++ src/main/java/com/sandropastelaria/omniorder/model/Pedido.java
@@ -1,5 +1,86 @@
 package com.sandropastelaria.omniorder.model;
 
-public class Pedido {
+import com.sandropastelaria.omniorder.enums.EstadoCozinha;
+import com.sandropastelaria.omniorder.enums.EstadoPedido;
 
+public class Pedido {
+    private Integer idPedido;
+    private EstadoPedido estadoPedido;
+    private EstadoCozinha estadoCozinha;
+    private Integer idMesa;
+
+    public Pedido() {
+        super();
+    }
+
+    public Pedido(Integer idPedido, String estadoPedido, String estadoCozinha) {
+        this.idPedido = idPedido;
+        this.estadoPedido = EstadoPedido.toEnum(estadoPedido);
+        this.estadoCozinha = EstadoCozinha.toEnum(estadoCozinha);
+    }
+
+    public Pedido(Integer idPedido, String estadoPedido, String estadoCozinha, Integer idMesa) {
+        this.idPedido = idPedido;
+        this.idMesa = idMesa;
+        this.estadoPedido = EstadoPedido.toEnum(estadoPedido);
+        this.estadoCozinha = EstadoCozinha.toEnum(estadoCozinha);
+    }
+
+    public Integer getIdPedido() {
+        return idPedido;
+    }
+
+    public void setIdPedido(Integer idPedido) {
+        this.idPedido = idPedido;
+    }
+
+    public String getEstadoPedido() {
+        return estadoPedido.getDescricao();
+    }
+
+    public void setEstadoPedido(String estadoPedido) {
+        this.estadoPedido = EstadoPedido.toEnum(estadoPedido);
+    }
+
+    public String getEstadoCozinha() {
+        return estadoCozinha.getDescricao();
+    }
+
+    public void setEstadoCozinha(String estadoCozinha) {        
+        this.estadoCozinha = EstadoCozinha.toEnum(estadoCozinha);
+    }
+
+    public Integer getIdMesa() {
+        return idMesa;
+    }
+
+    public void setIdMesa(Integer idMesa) {
+        this.idMesa = idMesa;
+    }
+
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + ((idPedido == null) ? 0 : idPedido.hashCode());
+        return result;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        Pedido other = (Pedido) obj;
+        if (idPedido == null) {
+            if (other.idPedido != null)
+                return false;
+        } else if (!idPedido.equals(other.idPedido))
+            return false;
+        return true;
+    }
+    
 }

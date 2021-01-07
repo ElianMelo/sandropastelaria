b20eeedb356c511a4e0622043c06f8dc9214bc2f
ElianMelo <elianmelomorais@gmail.com>
2021-01-06 22:21:25 -0300

    Realiza implementação da tela mesa

--- src/main/java/com/sandropastelaria/omniorder/controller/Omniorder.java
+++ src/main/java/com/sandropastelaria/omniorder/controller/Omniorder.java
@@ -6,16 +6,10 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class Omniorder {
 
-
     @RequestMapping("/")
     public String controllerLogin() {
         return "login";
     }
-    
-    @RequestMapping("/mesa")
-    public String controllerMesa() {
-        return "mesa";
-    }	
     
     @RequestMapping("/cozinha")
     public String controllerCozinha() {
